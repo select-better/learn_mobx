600f13a4e3e327c4dfd68439201276f7b4d51986
wcl <15728005026@163com>
2021-08-22 00:37:20 +0800

    feat:简便一点

--- src/comp/Comp.jsx
+++ src/comp/Comp.jsx
@@ -3,6 +3,11 @@ import { useCounter, useUpdate } from './utils'
 
 import './index.css'
 
+// 增加一个全局变量，记录需要加入的更新方式
+const globalState = {
+    currentReaction: null
+}
+
 // 原子，用于区分定义
 class Reaction{
     constructor(name, innerEffect){
@@ -14,6 +19,12 @@ class Reaction{
 // 原子上的电子，用来触发事件
 class Atom{
    reactions = new Set()
+   // 增加我们的原子
+   addReaction(){
+       if(globalState.currentReaction){
+           this.reactions.add(globalState.currentReaction)
+       }
+   }
    // 触发我们的更新
    actionReaction(){
        this.reactions.forEach(item=>item.innerEffect && item.innerEffect())
@@ -24,28 +35,24 @@ class Model{
     state_a = 222;
     atom_a = new Atom();
     // 当作数据来执行 比如get 和 set
-    get_a(reaction){
-        if(reaction){
-            this.atom_a.reactions.add(reaction)
-        }
+    get a(){
+        this.atom_a.addReaction();
         return this.state_a
     }
-    set_a(val){
-        // 这里顺序反了会慢一步更新
-       this.state_a = val
-       this.atom_a.actionReaction()
+
+    set a(val){
+        this.state_a = val;
+        this.atom_a.actionReaction();
     }
 
     state_b = 'bbb';
     atom_b = new Atom();
  
-    get_b(reaction){
-        if(reaction){
-            this.atom_b.reactions.add(reaction)
-        }
+    get b(){
+        this.atom_b.addReaction()
         return this.state_b
     }
-    set_b(val){
+    set b(val){
         this.state_b = val
         this.atom_b.actionReaction()
     }
@@ -59,25 +66,34 @@ const A = memo(() => {
     const count = useCounter();
     const updateFun = useUpdate();
     const [showReact]= useState(()=>new Reaction('aaa', updateFun));
-    return <div className='container'>
-        <div>title:A</div>
-        <button onClick={()=>model.set_a(count)}> 按钮 </button>
-        <div>value:{model.get_a(showReact)}</div>
-        <div> 更新的次数：{count} </div>
-    </div>
+    globalState.currentReaction = showReact
+    try{
+        return <div className='container'>
+            <div>title:A</div>
+            <button onClick={()=>model.a = count }> 按钮 </button>
+            <div>value:{model.a}</div>
+            <div> 更新的次数：{count} </div>
+        </div>
+    }finally{
+        globalState.currentReaction = null
+    }
 })
 
 const B = memo(() => {
     const count = useCounter();
     const updateFun = useUpdate();
     const [reactionB] = useState(()=>new Reaction('bbb', updateFun))
-
-    return <div className='container'>
-        <div>title:B</div>
-        <button onClick={()=>model.set_b(count)}> 按钮 </button>
-        <div> value: {model.get_b(reactionB)} </div>
-        <div> 更新的次数：{count} </div>
-    </div>
+    globalState.currentReaction = reactionB
+    try{
+        return <div className='container'>
+            <div>title:B</div>
+            <button onClick={()=>model.b = count}> 按钮 </button>
+            <div> value: {model.b} </div>
+            <div> 更新的次数：{count} </div>
+        </div>
+    }finally{
+        globalState.currentReaction = null
+    }
 })
 
 const Sum = () => {
